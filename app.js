8ba6b2e681c5b24ff74d46c7e532044a50e85c0d
lily <delanylily@gmail.com>
2023-01-24 08:33:02 +0100

    Added event listener for changeLocation

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const weather = new Weather('London');
 const ui = new UI()
 document.addEventListener('DOMContentLoaded', getWeather())
-
+document.getElementById('w-change-btn').addEventListener('click', (e) => {
+    const city = document.getElementById('city').value;
+    weather.changeLocation(city);
+    getWeather();
+})
 // weather.changeLocation('Miami');
 
 function getWeather() {
